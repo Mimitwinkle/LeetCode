c900e5b02d907eb376f5cac6483f5cd85a1d97e6
Kelly Hills <kellygreentea32@gmail.com>
2023-03-03 14:10:58 -0500

    Time: 738 ms (11.85%), Space: 43.6 MB (28.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
+        if(s.isEmpty()) return true;
+        
         String backwardS = "";
         
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
